368b85decc60b95a9e76926f1b04f97c9bd77486
Lobna <lobna.ghanim@campus.technion.ac.il>
2023-06-04 12:43:43 +0300

    first commit

---
+++ HealthPoints.h
@@ -0,0 +1,21 @@
+#ifndef HEALTHPOINTS_H
+#define HEALTHPOINTS_H
+#define maxHP = 100;
+class HealthPoints{
+
+
+public:
+
+    explicit HealthPoints(const int HP);
+
+
+private:
+
+
+
+
+};
+
+
+
+#endif
