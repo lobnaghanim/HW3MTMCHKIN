6034735623a0d567f84dd9e6b77e9548de81cc27
Ali <alli.harbaji@gmail.com>
2023-06-04 13:23:24 +0300

    third commit

---
+++ cmake-build-debug/CMakeFiles/HW3MTMCHKIN.dir/HealthPoints.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/HW3MTMCHKIN.dir/HealthPoints.cpp.obj: \
+ C:\Users\Ali\ Harbaji\Documents\GitHub\HW3MTMCHKIN\HealthPoints.cpp \
+ C:\Users\Ali\ Harbaji\Documents\GitHub\HW3MTMCHKIN\HealthPoints.h
---
+++ cmake-build-debug/CMakeFiles/HW3MTMCHKIN.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for HW3MTMCHKIN.
+# This may be replaced when dependencies are built.
