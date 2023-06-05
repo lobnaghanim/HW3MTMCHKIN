550d494485a62e99cf2f119fd50ed50cce06b060
Ali <alli.harbaji@gmail.com>
2023-06-05 22:33:17 +0300

    updated Queue.h

--- Queue.h
+++ Queue.h
@@ -14,6 +14,14 @@ private:
 
 public:
     Queue(): head(nullptr), tail(nullptr) {}
+    ~Queue() {
+        Node* t_ptr = head;
+        while(t_ptr != nullptr){
+            Node* t_ptr2 = t_ptr->next;
+            delete t_ptr;
+            t_ptr = t_ptr2;
+        }
+    }
 
     void pushBack(const T& element) {
         Node * newNode = new Node(element);
@@ -27,8 +35,21 @@ public:
         }
     }
 
-    Node* head;
+    T& front() const{
+        return head->data;
+    }
+
+    void popFront(){
+        if(head == nullptr){ return;}
+
+        Node* t_ptr = head->next;
+        delete head;
+
+        head = t_ptr;
+    }
+
     Node* tail;
+    Node* head;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -3,12 +3,25 @@ using namespace std;
 #include "HealthPoints.h"
 #include "Queue.h"
 int main(){
-    Queue<int> q1;
-    q1.pushBack(67);
-    q1.pushBack(68);
+    Queue<int> queue1;
+    queue1.pushBack(67);
+    queue1.pushBack(2);
+    queue1.pushBack(3);
 
-    printf("q1.head() = %d\n", q1.head->data);
-    printf("q1.tail() = %d\n", q1.tail->data);
+    int front1 = queue1.front(); /* front1 equals 1 as it's the first value */
+    printf("front1 = %d\n", front1);
+    queue1.front() = 1; /* queue1 content is: 3, 2 (left to right) */
+    front1 = queue1.front(); /* front1 now equals 3 */
+    printf("front1 = %d\n", front1);
 
+
+    printf("q1 first element: %d\n", queue1.front());
+    printf("q1 second element: %d\n", queue1.head->next->data);
+    printf("q1 third: %d\n", queue1.tail->data);
+
+    printf("-------popped front-------\n");
+    queue1.popFront(); /* queue1 content is: 2 */
+
+    printf("q1 first element: %d\n", queue1.front());
     return 0;
 }
